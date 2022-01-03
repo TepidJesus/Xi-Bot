f8fc7aa5b0d586a6361166869722ff1ed4a1778b
Thomas Holdom <holdomthomas@gmail.com>
2022-01-03 20:35:43 +1300

    Extended China Based Replies
    Continued work on the china good or bad reply system. Added more words to the bad word list and add a new praise words list to reward good things being said about China.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 import discord
 from discord import message
+from discord.abc import Messageable
 from dotenv import load_dotenv
 import random
+
 WELCOME_OPTIONS = ['愚蠢的西方人', '外国人', '鬼佬', '鬼子', '老外', '美国间谍',
 '资本家', '资本主义猪']
 
-
-
+FORBIDDEN_WORDS = ['bad', 'stupid', 'worse', 'hate', 'overthrow', 'awful', 'dreadful', 'poor', 'cheap', 'imperfect', 'sucks', 'suck', 'trash', 'garbage', 'dislike', 'shit', 'fuck', 'worst']
+PRAISE_WORDS = ['good', '#1', 'number 1', 'great', 'fucks']
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -25,8 +27,18 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
+    message_list = list()
+    message_list = message.content.split(' ')
+    bad_word_check = any(other_word in message_list for other_word in FORBIDDEN_WORDS)
+    praise_word_check = any(other_word in message_list for other_word in PRAISE_WORDS)
     if message.author == client.user:
         return
+    elif ('china' in message_list or 'China' in message_list) and bad_word_check:
+        response = '🇨🇳 This message has been reported to The Ministry of State Security 🇨🇳'
+        await message.channel.send(response)
+    elif ('china' in message_list or 'China' in message_list) and praise_word_check:
+        response = '🇨🇳 The People Of China Thank You For Your Kind Words. +100 Social Credit 🇨🇳'
+        await message.channel.send(response)
     elif message.content == 'China' or message.content == 'china':
         response = '🇨🇳 China #1 🇨🇳'
         await message.channel.send(response)
