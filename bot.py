e6101ce4c4b946a00639736c2a40dfe37e0c5d5e
Thomas Holdom <holdomthomas@gmail.com>
2022-01-08 21:57:44 +1300

    Updated To OOP Format
    Changed all code to better comply with the OOP format. All functionality has been retained and nothing new added since previous commit in terms of features.

---
+++ bot.py
@@ -0,0 +1,73 @@
+from dotenv import load_dotenv
+from discord.ext import commands
+import discord
+import os
+import random
+from requests import get
+from credit_keeper import CreditKeeper
+from message_analyzer import Message_processor
+
+
+WELCOME_OPTIONS = ['愚蠢的西方人', '外国人', '鬼佬', '鬼子', '老外', '美国间谍',
+'资本家', '资本主义猪', '安全威胁']
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+class XiBot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(command_prefix='!')
+        intents = discord.Intents.default()
+        intents.members = True
+        self.credit_score_keeper = CreditKeeper()
+        self.message_processor = Message_processor()
+
+        @self.command(name='my-credit')
+        async def my_credit(ctx):
+            scores = self.credit_score_keeper.get_credit_score(ctx.message.author.name)
+            await ctx.send(scores)
+
+        @self.command(name='server-ip', help='Displays the current IP of the Minecraft Server. Use in case of IP change.')
+        async def server_ip(ctx):
+            ip = get('https://api.ipify.org').text
+            await ctx.send(f'The Current Server IP is: {ip}')
+
+        @self.command(name='show-credit')
+        async def show_credit(ctx):
+            scores = self.credit_score_keeper.display_credit_scores()
+            await ctx.send(scores)
+    
+    async def on_ready(self):
+            print(f'{self.user} has properly joined discord...')
+            print(f'{self.user} Has Joined The Server {self.guilds[0]}')
+            self.credit_score_keeper.refresh_creditscores(guild_members=self.get_all_members())
+
+    async def on_member_join(self, member):
+        guild = member.guild
+        print(f'{member.name} Joined')
+        self.credit_score_keeper.refresh_creditscores(guild_members=self.get_all_members())
+        if guild.system_channel != None:
+            welcome_message = f'The {random.choice(WELCOME_OPTIONS)} {member.name} has joined the server!'
+            await guild.system_channel.send(welcome_message)
+        else:
+            return
+
+    async def on_message(self, message):
+            await bot.process_commands(message)
+            if message.author == self.user:
+                return
+            elif message.content.startswith('!'):
+                return
+            else:
+                messsage_list = self.message_processor.listify_message(message)
+                bad_word_check, praise_word_check, china_check = self.message_processor.run_message_checks(messsage_list)
+                bot_response = self.message_processor.choose_response(message = message, message_list= messsage_list, china_check = china_check, bad_word_check = bad_word_check, praise_word_check = praise_word_check)
+            if bot_response == None:
+                return
+            else:
+                await message.channel.send(bot_response)
+                
+bot = XiBot()
+bot.run(TOKEN)
---
+++ message_analyzer.py
@@ -0,0 +1,59 @@
+from credit_keeper import CreditKeeper
+
+FORBIDDEN_WORDS = ['bad', 'stupid', 'worse', 'hate', 'overthrow', 'awful', 'dreadful', 'poor', 'cheap', 'imperfect', 'sucks', 'suck', 'trash', 'garbage', 'dislike', 'shit', 'fuck', 'worst', 'terrible', 'dumb', 'cool', 'amazingly', 'stoopid', 'gey', 'sus', 'imposter', 'corrupt']
+PRAISE_WORDS = ['good', '#1', 'number 1', 'great', 'fucks', 'pog', 'poggers', 'best', 'amazing', 'love', 'china#1', 'superior', 'praise', 'very', 'predatorial']
+CHINA_WORDS = ['china', 'chinese']
+NEGATIONS = ['isn\'t', 'not', 'never', 'isnt']
+
+class Message_processor():
+    def __init__(self) -> None:
+        self.credit_score_keeper = CreditKeeper()
+
+    def listify_message(self, message_raw):
+        print(f'[MESSAGE]: {message_raw.author} Says \"{message_raw.content}\"')
+        self.message_list = list()
+        self.message_list = message_raw.content.split(' ')
+        for i in range(len(self.message_list)):
+            if self.message_list[i].isalpha():
+                self.message_list[i] = self.message_list[i].lower()
+            elif self.message_list[i].isalnum() != True:
+                self.message_list[i] = self.message_list[i].strip('!')
+                self.message_list[i] = self.message_list[i].strip('.')
+                self.message_list[i] = self.message_list[i].strip(',')
+                self.message_list[i] = self.message_list[i].strip('-')
+                self.message_list[i] = self.message_list[i].lower()
+            else:
+                continue
+        return self.message_list
+
+    def run_message_checks(self, message_list):
+        bad_word_check = any(other_word in message_list for other_word in FORBIDDEN_WORDS)
+        praise_word_check = any(other_word in message_list for other_word in PRAISE_WORDS)
+        china_check = any(other_word in message_list for other_word in CHINA_WORDS)
+
+        for i in range(len(message_list)):
+            if message_list[i] in PRAISE_WORDS and message_list[i-1] in NEGATIONS:
+                bad_word_check = True
+            elif message_list[i] in FORBIDDEN_WORDS and message_list[i-1] in NEGATIONS:
+                bad_word_check = False
+
+        return bad_word_check, praise_word_check, china_check
+
+    def choose_response(self, message, message_list, china_check, bad_word_check, praise_word_check):
+        if china_check and bad_word_check: # Punish
+            response = '🇨🇳 This message has been reported to The Ministry of State Security 🇨🇳\n🇨🇳 10 Credit Points Have Been Deducted From Your Balance 🇨🇳'
+            self.credit_score_keeper.alter_creditscore(member=message.author.name, points=-10)
+            return response
+        elif china_check and praise_word_check and bad_word_check != True: # Reward
+            response = '🇨🇳 The People Of China Thank You For Your Kind Words 🇨🇳\n🇨🇳 1 Credit Point Has Been Added To Your Balance 🇨🇳'
+            self.credit_score_keeper.alter_creditscore(member=message.author.name, points=1)
+            return response
+        elif 'taiwan' in message_list:
+            response = '🇨🇳 Did You Mean Chinese Taipei? 🇨🇳'
+            return response
+        elif china_check:
+            response = '🇨🇳 China #1 🇨🇳'
+            return response
+        else:
+            return None
+        
